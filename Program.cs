78328aca2311651fad61805f5bcee4c089a08bc9
Tim Nyman <timnyman@live.com>
2025-08-12 16:58:17 +0200

    Added Data and Service layer.
    Added Different entities to represent the different expenses.
    
    Made one GetAllExpenses and one AddExpense methods.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("ExpenseDb")));
 
 var app = builder.Build();
 
