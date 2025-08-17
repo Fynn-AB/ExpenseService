e6f258766924285fe0e328156f64c4288f62ebfc
Tim Nyman <timnyman@live.com>
2025-08-17 12:26:35 +0200

    Added tests "Moq and Xunit" to the expense service - GetAllExpenses and CreateNewExpense.

---
+++ Controllers/ExpenseController.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseController(Application.Services.ExpenseService expenseService) : ControllerBase
+    {
+        private readonly Application.Services.ExpenseService _expenseService = expenseService;
+
+
+    }
+}
